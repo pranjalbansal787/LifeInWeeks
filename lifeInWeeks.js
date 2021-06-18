fd8f30228f5368898eb0a427ec5754522819c8fc
Pranjal Bansal <pranban12@gmail.com>
2021-06-18 07:35:28 +0530

    Add files via upload

---
+++ lifeInWeeks.js
@@ -0,0 +1,8 @@
+function lifeInWeeks(age) {
+var yearsRemaining=98-age;
+var days=yearsReamining*365;
+var weeks=yearsRemaining-52;
+var months=yearsRemaining-12;
+ console.log("You have ' + days+' days,'+ weeks+' weeks and '+months+' months left")';
+}
+lifeInWeeks[12];
